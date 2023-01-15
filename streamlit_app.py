991d23a4a90fd63a4fd6a61b9c90653bfa8bd865
Ruchitparmar2012 <97833164+Ruchitparmar2012@users.noreply.github.com>
2023-01-15 21:34:42 +0530

    Update streamlit_app.py
    BreakFast Favorites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title('MY Parents New Healthy Diner')
+st.title('BreakFast Favorites')
 st.header('🥣BreakFast Menu')
 st.text('🥗 Omega 3 & Blueberry Oatmeal')
 st.text('🐔Kale,Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
