2063da11732d397c71656f5311f2cadfaa6b5991
Ruchitparmar2012 <97833164+Ruchitparmar2012@users.noreply.github.com>
2023-01-15 21:36:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 st.title('BreakFast Favorites')
 st.text('🥣BreakFast Menu')
 st.text('🥗 Omega 3 & Blueberry Oatmeal')
 st.text('🐔Kale,Spinach & Rocket Smoothie')
 st.text('🥑🍞Hard-Boiled Free-Range Egg')
-
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
