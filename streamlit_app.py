fceb30a55401fb6dcf0e0c09e2d8c24715dcf62a
Ruchitparmar2012 <97833164+Ruchitparmar2012@users.noreply.github.com>
2023-01-15 21:37:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title('BreakFast Favorites')
-st.text('🥣BreakFast Menu')
+st.text('🥣 BreakFast Menu')
 st.text('🥗 Omega 3 & Blueberry Oatmeal')
-st.text('🐔Kale,Spinach & Rocket Smoothie')
-st.text('🥑🍞Hard-Boiled Free-Range Egg')
+st.text('🐔 Kale,Spinach & Rocket Smoothie')
+st.text('🥑🍞 Hard-Boiled Free-Range Egg')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
